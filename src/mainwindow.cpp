a981e43d537cb87ffdee2f63695886fcb7c41dac
Nourhan Essam <nourhanessam94@gmail.com>
2015-11-27 03:49:49 +0200

    #6 - Testing Code removed from main

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,17 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "drawablegenericgate.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    Logicsim::Gate *gate = new Logicsim::Gate();
-    gate->setPos(0, 0);
-    QGraphicsScene *sc=new QGraphicsScene();
-    sc->addItem(gate);
-    ui->graphicsView->setScene(sc);
+
 }
 
 MainWindow::~MainWindow()
