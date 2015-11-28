7dd2f30e1ccefcfa1eea68a419827868575f03b2
Shaza-Is <shaza993@yahoo.com>
2015-11-28 22:56:57 +0200

    #5-removing testing code from main

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,6 @@ using namespace Logicsim;
 
 int main(int argc, char *argv[])
 {
-    /*
-    NotGate b;
-    //b.setMaxInput(2);
-    QList<Node*> n;
-
-    Node* m;
-    m->setValue(false);
-    n.append(m);
-    b.setInput(n);
-    b.calcOutput();
-    bool c = (b.outputNode())->getValue();
-
-
-    qDebug() << c;*/
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
