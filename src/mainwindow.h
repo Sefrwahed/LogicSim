baa14fbd284e39385e90c320ffa8a7d3871f9206
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2015-12-08 22:15:08 +0200

    #18 - Adding Gray color to the main frame when no active tabs

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -21,6 +21,7 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     void initComponentsTab();
+    void setMainFrameDisabled(bool disabled);
 
 public Q_SLOTS:
     void newFile();
