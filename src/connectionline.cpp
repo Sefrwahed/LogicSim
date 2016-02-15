a76bc66f0c08a00fbb28104d1ae4136ba9c7f6d2
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2016-02-15 21:03:30 +0200

    #24 - Prettier connection line

--- src/connectionline.cpp
+++ src/connectionline.cpp
@@ -9,9 +9,8 @@ namespace Logicsim
 ConnectionLine::ConnectionLine(const QLineF &line,QGraphicsItem* parent)
     : QGraphicsLineItem(line, parent), m_out(0), m_in(0)
 {
-    QPen p;
-    p.setWidthF(2);
-    setPen(p);
+    setAcceptHoverEvents(true);
+//    setToolTip(m_out->toolTip() + " <-> " + m_in->toolTip());
     setFlag(QGraphicsItem::ItemIsSelectable);
 }
 
@@ -32,17 +31,25 @@ Pin *ConnectionLine::input() const
 void ConnectionLine::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     painter->setRenderHint(QPainter::Antialiasing);
-    QGraphicsLineItem::paint(painter, option, widget);
+
+    QPen p;
+    p.setWidthF(2);
+    p.setStyle(Qt::DashDotDotLine);
+    p.setCapStyle(Qt::RoundCap);
+    p.setJoinStyle(Qt::MiterJoin);
 
     if(isSelected())
     {
-        QPen p;
-        p.setWidthF(2);
-        p.setColor(QColor(0,191,255));
-        painter->setPen(QColor(Qt::blue));
-        painter->drawLine(line());
+        p.setColor(QColor(30,144,255));
+        setPen(p);
+    }
+    else
+    {
+        p.setColor(Qt::black);
+        setPen(p);
     }
 
+    QGraphicsLineItem::paint(painter, option, widget);
 }
 
 } // namespace Logicsim
